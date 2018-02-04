133bc9482dd7d8250c6530953347e3d9344eb053
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-04 20:09:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
-  def initialize(name,genre)
+  def initialize(name)
     @name=name
-    @genre=genre
   end
   def artist_name
     self.artist.name
